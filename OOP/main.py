9d7e6aa2cfafb2cda66bdafaf138dbbc560efed6
Code-Moises <becerramoralesmoises24@gmail.com>
2025-10-29 12:49:41 +0100

    OOP variables exercise done

--- OOP/main.py
+++ OOP/main.py
@@ -1,12 +1,13 @@
 from car import Car
+from student import Student
 
-# let's create a car
-
+# let's create a cars
 car1 = Car("Mustang", 2024, "red", False)
 car2 = Car("Corvette", 2025, "blue", True)
 car3 = Car("Charger", 2026, "yellow", True)
 
-print(car1.model)
-print(car1.year)
-print(car1.color)
-print(car1.for_sale)
+student1 = Student("SpongeBob", 30)
+student2 = Student("Patrick", 35)
+
+print(student1.class_year)
+print(Student.num_students)
